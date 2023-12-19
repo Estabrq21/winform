dac71ed693df5b9a31ff855645e4796f52cca02d
استبرق بلال عبدالباسط عثمان <istabraq.bilal.ai@mau.edu.eg>
2023-12-20 01:03:59 +0200

    Add files via upload

---
+++ media.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO Libraries (Title, YearReleased, Rating, Viewss, Typee, Languages, Cat_Name)
+VALUES
+   ('Night Watch', 2022, 4, 1200, 'Drama', 'Russian', 'Movies'),
+    ('Day Watch', 2023, 5, 1400, 'Romance', 'Russian', 'Movies'),
+    -- Add more Russian movies as needed
+    ('Leviathan', 2024, 3, 1100, 'Comedy', 'Russian', 'Series'),
+    ('Stalker', 2025, 4, 1300, 'Thriller', 'Russian', 'Series'),
+    -- Add more Russian series as needed
+    ('Brigada', 2026, 5, 1500, 'Adventure', 'Russian', 'Animation'),
+    ('The Method', 2027, 4, 1000, 'Fantasy', 'Russian', 'Animation'),
+	 ('The Sniffer', 2002, 3, 900, 'Action', 'Russian', 'Movies'),
+    ('Hedgehog in the Fog', 2001, 4, 1100, 'Sci-Fi', 'Russian', 'Movies'),
+    -- Add more Russian movies as needed
+    ('The Cat Who Walked by Herself ', 2003, 5, 1200, 'Drama', 'Russian', 'Series'),
+    ('The Ugly Duckling', 2002, 4, 1000, 'Mystery', 'Russian', 'Series'),
+    -- Add more Russian series as needed
+    ('Nu, Pogodi!', 2001, 4, 1300, 'Fantasy', 'Russian', 'Animation'),
+    ('Masha and the Bear ', 2003, 5, 1400, 'Family', 'Russian', 'Animation');
+
+	select*from libraries
